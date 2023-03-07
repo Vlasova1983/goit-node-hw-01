51b3ea13d1e89ecdd082331c369ebcc3ca26d46c
Власова Юлия <106443520+Vlasova1983@users.noreply.github.com>
2023-03-07 10:50:16 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -2,54 +2,66 @@ const path = require('path');
 const fs = require('fs').promises;
 const contactsPath=path.join('db','contacts.json');
 
-const listContacts = async()=>{
+
+
+const readFileContacts = async()=>{
   try{
-    const data = await fs.readFile(contactsPath)   
-    console.table (JSON.parse(data));
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);     
+  } catch (err){
+    console.error(err)
+  }
+}
+
+const writeFileContacts= async(contacts)=>{
+  try{
+    const data=JSON.stringify(contacts);
+    await fs.writeFile(contactsPath,data);      
   } catch (err){
     console.error(err)
   }
 }
 
 
-const  getContactById = async (contactId)=> {  
-  try{
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);    
-    const contact = contacts.filter((contact)=>contact.id.includes(contactId))
-    console.log(contact);
-  } catch (err){
-    console.error(err)
-  }  
+const listContacts =async()=>{ 
+  const contacts = await readFileContacts();   
+  console.table (contacts);   
+}
+
+const  getContactById = async (contactId)=> {   
+  const contacts = await readFileContacts();
+  const contact =  contacts.filter((contact)=>contact.id.includes(contactId))
+  console.table(contact);  
+}
+
+function getRandomId(min, max) {
+  return `${Math.floor(Math.random() * max) + min}`
 }
 
 const  addContact= async(name, email, phone)=> {
-  try{    
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);    
-    const contact ={"name":`${name}`,"email":`${email}`,"phone":`${phone}`};
-    contacts.push(contact);
-    console.table (contacts);
-   await fs.writeFile(contactsPath,JSON.stringify(contacts));  
-  } catch (err){
-  console.error(err)
-  }   
+  const newContact ={"id":getRandomId(1,100),"name":`${name}`,"email":`${email}`,"phone":`${phone}`};
+
+  const contacts = await readFileContacts();      
+  contacts.push(newContact);
+
+  await writeFileContacts(contacts); 
+  console.table (contacts);
 } 
 
 
-const removeContact = async(contactId)=> { 
- try{
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);    
-  const newContact =[];
-  contacts.map((contact)=> {if (contact.id!==contactId){
-    newContact.push(contact)
-  }})  
-  console.table(newContact);
-   await fs.writeFile(contactsPath,JSON.stringify(newContact));   
-  } catch (err){
-  console.error(err)
-  } 
+const removeContact = async(contactId)=> {
+  const newListContacts =[];
+
+  const contacts = await readFileContacts();  
+
+  contacts.map((contact)=> {
+    if (contact.id!==contactId){
+    newListContacts.push(contact)
+    }
+  }) 
+
+  await writeFileContacts(newListContacts);
+  console.table(newListContacts);
 }
   
 
